c46f16c4f6ad9162d06e3eb53cee0919ffacbbe3
fdeluna <fernando.deluna.romero@gmail.com>
2016-06-06 20:41:24 +0200

    Rename MapGenerator to Map

---
+++ include/map/Map.h
@@ -0,0 +1,37 @@
+#ifndef Map_H
+#define Map_H
+
+#include "GameObject.h"
+#include "Node.h"
+#include "Enum.h"
+#include "Utils.h"
+
+#include <Ogre.h>
+#include <random>
+
+class Map : public GameObject{
+
+public:
+
+	Map(Ogre::SceneManager* sceneManager) :GameObject(sceneManager){};
+	~Map(){ cleanMap();  GameObject::~GameObject(); };
+
+	void GenerateMap();
+	void cleanMap();
+
+	Ogre::Vector2 _mapCenter;
+
+	std::vector< std::vector <Node*>> getGrid(){ return grid; };
+
+private:
+	Ogre::Vector2 _mapSize;
+
+	SceneNodeComponent* planeNode;
+	RigidBodyComponent* rigidBodyComponent;
+	std::vector< std::vector <Node*>> grid;
+
+	bool isMapAccessible(std::vector< std::vector <bool>> obstacleMap, int currentNumberObstacle);
+
+};
+
+#endif
